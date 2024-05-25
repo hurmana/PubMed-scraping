8da5089c6a7e20c4651e107674182b3104bf684f
hurma <mhuruhashi4@gmail.com>
2024-05-25 13:45:25 +0900

    Update PubMed_scraping.py
    Delete Options

--- PubMed_scraping.py
+++ PubMed_scraping.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import datetime, requests
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
